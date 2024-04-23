9bf21a8355dc061bb2a36b28d6b8d3ded6ceb4e0
Noman Shah <nomankhalidofficial@gmail.com>
2024-04-23 17:50:55 +0500

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+    <div className=" min-h-screen container bg-slate-800 flex items-center justify-center">
+    <div className=" text-center">
+      <h1 className=" text-[65px] font-bold flex text-white ">Hello World</h1>
+      <h2 className=" text-3xl text-slate-300">Noman Shah</h2>
+    </div>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
